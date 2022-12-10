23e16f618d9803d21a1677b8678a0a9940e995c8
Josh-Clemons <110069447+Josh-Clemons@users.noreply.github.com>
2022-12-10 10:14:18 -0600

    css updates, primarily to movie detail page

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import './Detail.css';
 
 const Detail = () => {
     const history = useHistory();
     const movie = useSelector(store => store.detail)
+
+    useEffect(()=> {
+        window.scrollTo(0,0)
+    }, []);
+
     return (
-        < div onClick={()=>{history.push('/')}}>
-            <div>{movie.title}</div>
+        < div className="detailDiv">
+            <h2>{movie.title}</h2>
             <img src={movie.poster} />
             <br />
-            <div>{movie.description}</div>
-            <ul>Categories:
+            <div className='movieDetailDiv'><span>Movie Details: </span>{movie.description}</div>
+            <ul><b>Categories:</b>
                 {movie.category_array.map((category) => {
                     return <li key={category}>{category}</li>
                 })}
             </ul>
-            <h3>HERE IS A THING TO CLICK ON TO GO BACK, but not necessary</h3>
+            <div className="backButtonFixed" onClick={()=>{history.push('/')}}>Go Back</div>
 
         </div>
     )
